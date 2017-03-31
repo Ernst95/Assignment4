f46169f137604c295a7b57e8fb30728efa843e33
Ernst95 <ernst.chapman@gmail.com>
2017-03-31 22:13:40 +0200

    Sixth Commit

--- Question4/Obeyed06/Display/src/test/java/com/Assignment4/DisplayTest.java
+++ Question4/Obeyed06/Display/src/test/java/com/Assignment4/DisplayTest.java
@@ -46,7 +46,7 @@ public class DisplayTest {
 
     @Test
     public void confirmationMessage() throws Exception {
-      
+
     }
 
 }
---
+++ Question4/Obeyed06/src/main/java/com/Assignment/runApp.java
@@ -0,0 +1,16 @@
+package com.Assignment;
+
+import com.Assignment4.Display;
+
+/**
+ * Created by Ernst on 2017/03/31.
+ */
+public class runApp {
+
+        public static void main( String[] args )
+        {
+            Display dis = new Display();
+            dis.confirmationMessage();
+        }
+    
+}
